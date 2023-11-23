6d4e44596c8e18070edbbd56d1ab309b6e7649c2
root <root@Linux.myguest.virtualbox.org>
2023-11-23 13:09:45 +0300

    lesson3

--- lesson2/fibo.rb
+++ lesson2/fibo.rb
@@ -1,10 +1,10 @@
-fn = [0,1]
+fn = [0, 1]
 
 while fn.last <= 100
-	new_number = fn[-2] + fn[-1]
+  new_number = fn[-2] + fn[-1]
 	break if new_number >= 100
 
-	fn<<new_number
+	fn << new_number
 
 end
 
