00ee78f7f1e7fdb5667159d61b9fe8821679e65b
root <root@Linux.myguest.virtualbox.org>
2023-12-02 16:22:15 +0300

    pravki

---
+++ Railway_control_system/main.rb
@@ -0,0 +1,9 @@
+require_relative 'train/train'
+require_relative 'train/cargo_train'
+require_relative 'train/passenger_train'
+require_relative 'wagons/wagons'
+require_relative 'wagons/cargo_wagon'
+require_relative 'wagons/passenger_wagon'
+require_relative 'route'
+require_relative 'station'
+
---
+++ Railway_control_system/train/train.rb
@@ -0,0 +1,79 @@
+class Train
+	attr_reader :speed, :wagons
+
+	def initialize(car_number, type)
+		@car_number = car_number
+		@type = type
+		@wagons = []
+		@speed = 0
+	end
+
+	def up_speed
+		@speed += 10
+		puts "скорость увеличена на 10"
+	end
+
+	def stop
+		@speed = 0
+	end
+
+	def attach_wagon(wagon)
+		if @speed.zero?
+			@wagons << wagon 
+			puts "Добавлен один вагон. Всего вагонов: #{wagons.size}" 
+		else
+			puts "Вагон в движении, прицепить невозможно."
+		end
+	end
+
+	def detach_wagon 
+		if @speed.zero? && wagons.any?
+			wagons.pop
+			puts "Вагон отцеплен. Всего вагонов: #{wagons.size} "
+		else
+			puts "Вагон в движении, отцепить невозможно, или вагонов нет."
+		end
+	end
+
+	def set_route(route)
+    @route = route
+    @current_station_index = 0
+    current_station.add_train(self)
+  end
+
+  def move_forward
+    if next_station
+    	current_station.departure_train(self)
+      @current_station_index += 1
+      current_station.add_train(self)
+      puts "Перемещение вперед. Текущая станция: #{current_station.name}"
+    else
+      puts "Нельзя двигаться вперед, конечная станция."
+    end
+  end
+
+  def move_backward
+    if previous_station
+    	current_station.departure_train(self)
+      @current_station_index -= 1
+      current_station.add_train(self)
+      puts "Перемещение назад. Текущая станция: #{current_station.name}"
+    else
+      puts "Нельзя двигаться назад, начальная станция."
+    end
+  end
+
+  def current_station
+    @route.stations[@current_station_index]
+  end
+
+  def next_station
+    @route.stations[@current_station_index + 1]
+  end
+
+  def previous_station
+    @route.stations[@current_station_index - 1]
+  end
+
+
+end
